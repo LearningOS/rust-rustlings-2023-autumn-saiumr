5d49943f84eb7792123e0ebc416f20eda2a03d40
saiumr <colaloveanan@gmail.com>
2023-11-13 17:32:37 +0800

    util primitive type.

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,15 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    // let a: [i32; 3]; error
+    // let a: [i32; 3] = [0]; error
+    let a: [i32; 3] = [0; 3];
+    // let a = [0; 101];
+    // let a: [i32; 3] = [1, 2, 3]; 
+    // let a: [i32; 3] = todo!();
+    // let a = [1,2,3];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(apple_num: i32) -> i32 {
+    if apple_num <= 40 {
+        apple_num * 2
+    } else {
+        apple_num
+    }
+}
 
 // Don't modify this function!
 #[test]
